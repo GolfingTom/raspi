5c6ec880b88a4b5cd7591002fdaddb83f3ac619c
GolfingTom <twvitez@gmail.com>
2018-12-08 17:35:56 -0500

    Create mytest_of_2_gpios.py

---
+++ mytest_of_2_gpios.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+# NeoPixel library strandtest example
+# Author: Tony DiCola (tony@tonydicola.com)
+#
+# Direct port of the Arduino NeoPixel library strandtest example.  Showcases
+# various animations on a strip of NeoPixels.
+
+# sudo python3 mytest.py   to run
+
+import time
+from neopixel import *
+import argparse
+import math
+
+# LED strip configuration:
+LED_COUNT      = 300  # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+LED_PIN2        = 10     # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+
+
+def samecolorWipe(color, wait_ms=50, delay_ms=0):
+    """Wipe color across display a pixel at a time."""
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip2.setPixelColor(i, color)
+        strip.show()
+        strip2.show()
+
+def differentcolorWipe(color1, color2, wait_ms=50, delay_ms=0):
+    """Wipe color across display a pixel at a time."""
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color1)
+        strip2.setPixelColor(i, color2)
+        strip.show()
+        strip2.show()
+
+    
+# Main program logic follows:
+if __name__ == '__main__':
+    # Process arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
+    args = parser.parse_args()
+
+    # Create NeoPixel object with appropriate configuration.
+    strip = Adafruit_NeoPixel(LED_COUNT, 18, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    strip2 = Adafruit_NeoPixel(LED_COUNT, 21, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
+    strip2.begin()
+    
+    print ('Press Ctrl-C to quit.')
+
+    if not args.clear:
+        print('Use "-c" argument to clear LEDs on exit')
+
+    try:
+
+        while True:
+            samecolorWipe(Color(255,0,0), 0)
+            differentcolorWipe(Color(0,0,255), Color(0,255,0), 0)
+  
+            
+    
+
+    except KeyboardInterrupt:
+        if args.clear:
+            strip = Adafruit_NeoPixel(1000, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+            strip.begin()
+            colorWipe(strip, Color(0,0,0), 0)
